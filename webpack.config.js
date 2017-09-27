efe4808630660fd9cb837705bae779792934a343
Vincent Liu <vxiao2046@gmail.com>
2017-09-27 11:35:21 +0800

    static file and fronded build task

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+var path = require('path');
 module.exports = {
-  entry: '/src/index.js', 
+  entry: path.resolve(__dirname, 'src') + '/index.js',
   output: {
-    path: __dirname + 'dist', 
-    filename: 'bundle.js', 
-  }, 
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
   module: {
     loaders: [
       {
