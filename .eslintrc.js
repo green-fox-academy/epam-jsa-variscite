a09b0aa32e9a2886d01ed79d0642a6c5a02b4c9e
Vincent Liu <vxiao2046@gmail.com>
2017-09-28 11:40:31 +0800

    refine eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
 module.exports = {
-    "extends": "google"
+  'env': {
+    'es6': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true,
+    },
+    'sourceType': 'module',
+  },
+  'rules': {
+      'indent': [2,2,]
+  },
+  'plugins': [
+    'react',
+  ],
 };
