f502f8018e1dee7d1e68d72660387e26f6a36f40
Vincent Liu <vxiao2046@gmail.com>
2017-10-09 15:43:34 +0800

    page improve

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 const ReactDOM = require('react-dom');
 const React = require('react');
 import SignupComponent from './signupComponent.js';
+import './css/style.scss';
 
 ReactDOM.render(<SignupComponent />, document.getElementById('root'));
--- src/client/signupComponent.js
+++ src/client/signupComponent.js
@@ -3,14 +3,23 @@ var React = require('react');
 var SignupComponent = React.createClass({
   render: function () {
     return (
-      <div className="signup-form">
-        <h1>Create a New Account</h1>
-        <label>Username<input type="text" className="username" placeholder="Username"/></label>
-        <label>Email<input type="text" className="email" placeholder="Email"/></label>
-        <label>Phone number<input type="text" className="phonenumber" placeholder="Phone number"/></label>
-        <label>Full name<input type="text" className="fullname" placeholder="Full name"/></label>
-        <label>Password<input type="text" className="password" placeholder="Password"/></label>
-        <button className="signup">Sign up</button>
+      <div>
+        <header>
+          <button className="logo">V</button>
+          <button className="login">Log In</button>
+        </header>
+
+        <div className="signup-form">
+          <h1>Create a New Account</h1>
+          <input type="text" className="username" placeholder="Username"/><span>*</span>
+          <input type="text" className="email" placeholder="Email"/><span>*</span>
+          <input type="text" className="phonenumber" placeholder="Phone number"/>
+          <input type="text" className="fullname" placeholder="Full name"/>
+          <input type="text" className="password" placeholder="Password"/><span>*</span>
+          <button className="signup">Sign up</button>
+        </div>
+
+
       </div>
     );
   }
