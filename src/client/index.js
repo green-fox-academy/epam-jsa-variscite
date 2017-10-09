3e85049016608a38ca6b25c9b32678d76d519b5f
aliceshen227 <Alice_Shen1@epam.com>
2017-10-09 15:40:00 +0800

    added header component

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,15 @@
-const ReactDOM = require('react-dom');
-ReactDOM.render(<p>Hello World!</p>, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
