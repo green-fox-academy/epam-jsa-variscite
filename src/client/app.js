bf5e5140954ec6ff9c9a88c93b133dfd3bc6225f
Vincent Liu <vxiao2046@gmail.com>
2017-10-11 10:13:57 +0800

    app.js fix

--- src/client/app.js
+++ src/client/app.js
@@ -1,20 +1,14 @@
- import ReactDOM from 'react-dom';
- import React from 'react';
- import ReactDOM from 'react-dom';
- import {BrowserRouter as Router, Route} from 'react-router-dom';
- import Signup from './screens/SignupScreen/index.js';
- 
- class App extends React.Component {
-   render() {
-     return (
-       <Router>
-         <div>
-           <Route exact path='/' component={Signup} />
-           <Route path='/signup' component={Signup} />
-         </div>
-       </Router>
-     );
-   }
- }
- 
- export default App;
+import React from 'react';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header isLoggedIn={true}/>
+      </div>
+    );
+  }
+}
+
+export default App;
