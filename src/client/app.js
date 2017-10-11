9d52ebc4e69d6c2eb2a4a2755717cfea62450adb
Vincent Liu <vxiao2046@gmail.com>
2017-10-11 10:15:39 +0800

    app.js

--- src/client/app.js
+++ src/client/app.js
@@ -1,14 +1,20 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
-import Header from './components/Header';
-
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Signup from './screens/SignupScreen/index.js';
+ 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header isLoggedIn={true}/>
-      </div>
+      <Router>
+        <div>
+          <Route exact path='/' component={Signup} />
+          <Route path='/signup' component={Signup} />
+        </div>
+      </Router>
     );
   }
 }
-
+ 
 export default App;
