58a317e80e0afca71a36bcc746de518bf004ded4
aliceshen227 <Alice_Shen1@epam.com>
2017-10-12 20:11:23 +0800

    finish creating the test and they all passed

--- src/server/signUpErrorHandle.js
+++ src/server/signUpErrorHandle.js
@@ -6,19 +6,19 @@ function signUpErrorHandle(req, res) {
   let errorExists = false;
   if (req.headers['content-type'] !== 'application/json') {
     errorExists = true;
-    res.status(400).json({errorType: 'contentType'});
+    res.status(400).send({errorType: 'contentType'});
   } else if (req.body.email.length === 0) {
     errorExists = true;
-    res.status(400).json({errorType: 'missingEmail'});
+    res.status(400).send({errorType: 'missingEmail'});
   } else if (req.body.password.length === 0) {
     errorExists = true;
-    res.status(400).json({errorType: 'missingPassword'});
+    res.status(400).send({errorType: 'missingPassword'});
   } else if (req.body.email.length !== 0 && !emailRe.test(req.body.email)) {
     errorExists = true;
-    res.status(400).json({errorType: 'emailWrong'});
+    res.status(400).send({errorType: 'emailWrong'});
   } else if (req.body.password.length < 8 && req.body.password.length !== 0) {
     errorExists = true;
-    res.status(400).json({errorType: 'passwordWrong'});
+    res.status(400).send({errorType: 'passwordWrong'});
   }
   return errorExists;
 }
