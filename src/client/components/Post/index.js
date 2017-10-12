d26fa4850578c295abd3c62586f870fb2fbde8e6
Chase <chase.wang@hotmail.co.uk>
2017-10-13 00:02:44 +0800

    VRC-33

--- src/client/components/Post/index.js
+++ src/client/components/Post/index.js
@@ -10,8 +10,8 @@ class Post extends React.Component {
       <div className='postContainer'>
         <main className='postBox'>
           <PostHeader />
-          <p className='postText'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
-          <img className='postPic' src='https://fb-s-b-a.akamaihd.net/h-ak-fbx/v/t31.0-8/981699_614520911958065_621055260817408593_o.jpg?oh=32b13d341441699c05d16dfa69e04abb&oe=5A806D0F&__gda__=1518039321_65071f83d0dca5478896402ca4d80411' />
+          <p className='postText'>Make America great again! #America #greatwall</p>
+          <img className='postPic' src='http://ronpaulinstitute.org/media/121032/donald-trumps-mexico-border-wall-557313.jpg' />
           <PostInfo />
           <PostAct />
         </main>
