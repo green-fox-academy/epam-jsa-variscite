65c29817583102168bb8ae62a9289c79f0926ce2
Chase <chase.wang@hotmail.co.uk>
2017-10-13 11:56:35 +0800

    use state to declare variables

--- src/client/components/Comment/index.js
+++ src/client/components/Comment/index.js
@@ -4,13 +4,21 @@ import CommentInfo from '../CommentInfo/';
 import CommentInput from '../CommentInput/';
 
 class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Username: 'Hillary Clinton',
+      CommentTime: '10th Oct at 11:42PM',
+      UserPicURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk9HKJuqE3ZmpAWaWHEbFAvsCsktkwEFZ-aNKy9eo1VGvTh_hE',
+    };
+  }
   render() {
     return (
       <div className='commentContainer'>
         <div className='commentBox'>
-          <img className='userPic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk9HKJuqE3ZmpAWaWHEbFAvsCsktkwEFZ-aNKy9eo1VGvTh_hE' />
+          <img className='userPic' src={this.state.UserPicURL} />
           <div className='commentInfo'>
-            <CommentInfo />
+            <CommentInfo CommentTime={this.state.CommentTime}/>
           </div>
         </div>
         <CommentInput />
--- src/client/components/CommentInfo/index.js
+++ src/client/components/CommentInfo/index.js
@@ -7,7 +7,7 @@ class CommentInfo extends React.Component {
         <p className='text'><span className='name'>Hilary Clinton </span> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliqt, sed diam vtua. At accusam et.</p>
         <button className='likeButton'>Like</button><span> · </span>
         <button>Reply</button><span> · </span>
-        <p className='time'>10th Oct at 11:48PM</p>
+        <p className='time'>{this.props.CommentTime}</p>
       </div>
     );
   }
