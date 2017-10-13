9276891abe22e056b547572ba4fac5f310aeba25
Chase <chase.wang@hotmail.co.uk>
2017-10-13 11:38:48 +0800

    change font-family

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Post from './components/Post';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header isLoggedIn={true}/>
+        <Post />
       </div>
     );
   }
