6c946eb01c34c10b90ca5c58c7c470881aada5c7
Chase <chase.wang@hotmail.co.uk>
2017-10-13 15:04:55 +0800

    VRC-33

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import FeedPage from './screens/feed';
-import Signup from './screens/SignupScreen/index.js';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={Signup} />
+          <Route exact path='/' component={FeedPage} />
           <Route path='/feed' component={FeedPage} />
-          <Route path='/signup' component={Signup} />
         </div>
       </Router>
     );
