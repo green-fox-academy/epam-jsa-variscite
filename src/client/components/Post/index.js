44c343c466f656617608bdda86983a41330505f2
Chase <chase.wang@hotmail.co.uk>
2017-10-13 10:50:51 +0800

    fixed button background with text

--- src/client/components/Post/index.js
+++ src/client/components/Post/index.js
@@ -8,13 +8,13 @@ class Post extends React.Component {
   render() {
     return (
       <div className='postContainer'>
-        <main className='postBox'>
+        <section className='postBox'>
           <PostHeader />
           <p className='postText'>Make America great again! #America #greatwall</p>
           <img className='postPic' src='http://ronpaulinstitute.org/media/121032/donald-trumps-mexico-border-wall-557313.jpg' />
           <PostInfo />
           <PostAct />
-        </main>
+        </section>
       </div>
     );
   }
