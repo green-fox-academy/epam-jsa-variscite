fbd2eb5aa46ff5397ded479ab55262e93b13d7c0
Chase <chase.wang@hotmail.co.uk>
2017-10-13 10:56:09 +0800

    remove the post component from App.js for test

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
-import Post from './components/Post';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header isLoggedIn={true}/>
-        <Post />
       </div>
     );
   }
