88edd8f0c9ed0abcfce09c5b0e68ace9dbdd0b37
Chase <chase.wang@hotmail.co.uk>
2017-10-14 13:36:29 +0800

    some font-size

--- src/client/components/CommentInfo/index.js
+++ src/client/components/CommentInfo/index.js
@@ -7,7 +7,7 @@ class CommentInfo extends React.Component {
         <p className='text'><span className='name'>{this.props.CommentUsername} </span> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliqt, sed diam vtua. At accusam et.</p>
         <button className='likeButton'>Like</button><span> · </span>
         <button>Reply</button><span> · </span>
-        <p className='time'>{this.props.CommentTime}</p>
+        <span className='time'>{this.props.CommentTime}</span>
       </div>
     );
   }
