f4b9a7b8d2a626e337c4a9940654e3cf996d2fe8
Vincent Liu <vxiao2046@gmail.com>
2017-10-16 18:14:50 +0800

    separate functions

--- src/server/collections/usersDatabase.js
+++ src/server/collections/usersDatabase.js
@@ -6,79 +6,118 @@ const Cryptr = require('cryptr');
 const cryptr = new Cryptr('jiaMi');
 const tokenHandler = require('../modules/tokenHandler.js');
 
+function checkInfo(obj, userAgent, password, res) {
+  let encrypted = cryptr.encrypt(password);
+  if (obj !== null && obj.password === encrypted) {
+    tokenHandler.createAccessToken(obj._id,
+      userAgent, function (err, tokenDescriptor) {
+        let tokenObj = {
+          'token': tokenDescriptor.token,
+          'expiresAt': tokenDescriptor.expiresAt,
+        };
+        res.status(200).json(tokenObj);
+      });
+  } else {
+    res.status(403).json({'errorType': 'MisMatch'});
+  }
+}
+
+function retrieveUserInfo(db, userAgent, email, password, res) {
+  db.collection('users').findOne({email: email}, (err, obj) => {
+    if (err !== null) {
+      console.log('Couldn\'t get element from the db', err);
+      return;
+    }
+    checkInfo(obj, userAgent, password, res);
+  });
+}
+
 function checkUserInfo(userAgent, email, password, res) {
-  MongoClient.connect(url, function(err, db) {
-    db.collection('users').findOne({email: email}, function(err, obj) {
-      let encrypted = cryptr.encrypt(password);
-      console.log(encrypted);
-      if (obj !== null && obj.password === encrypted) {
-        tokenHandler.createAccessToken(obj._id,
-          userAgent, function(err, tokenDescriptor) {
-            let obj = {
-              'token': tokenDescriptor.token,
-              'expiresAt': tokenDescriptor.expiresAt,
-            };
-            res.status(200).json(obj);
-          });
-      } else {
-        res.status(403).json({'errorType': 'MisMatch'});
+  MongoClient.connect(url, (err, db) => {
+    if (err !== null) {
+      console.log('Couldn\'t get connect to the db', err);
+      return;
+    }
+    retrieveUserInfo(db, userAgent, email, password, res);
+  });
+}
+
+function sendStatus(req, res) {
+  let objectId = req._id;
+  res.set('location', '/api/signup/' + objectId);
+  res.status(201).json();
+  return;
+}
+
+function insertUser(db, req, res) {
+  db.collection('users').insert(req, function(err) {
+    if (err) {
+      res.status(500).json({errorType: 'serverError'});
+      console.log('Couldn\'t insert the element into the db', err);
+      return;
+    }
+    sendStatus(req, res);
+  });
+}
+
+function handleEmailError(db, res) {
+  res.status(409).json({errorType: 'emailError'});
+  db.close();
+}
+
+function handleUsernameError(db, res) {
+  res.status(409).json({errorType: 'usernameError'});
+  db.close();
+}
+
+function handlePhonenumberError(db, res) {
+  res.status(409).json({errorType: 'phoneError'});
+  db.close();
+}
+
+function handleError(db, item, req, res) {
+  if (item.email === req.email) {
+    handleEmailError(db, res);
+  } else if (item.username === req.username && item.username !== '') {
+    handleUsernameError(db, res);
+  } else if (item.phonenumber === req.phonenumber) {
+    handlePhonenumberError(db, res);
+  }
+}
+  
+function checkField(db, item, req, res) {
+  if (item === null) {
+    insertUser(db, req, res);
+  } else {
+    handleError(db, item, req, res);
+  }
+}
+
+function findUser(db, req, res) {
+  db.collection('users').findOne({
+    $or: [
+      {$and: [{'username': req.username}, {'username': {$ne: ''}}]},
+      {'email': req.email},
+      {$and: [{'phonenumber': req.phonenumber}, {'phonenumber': {$ne: ''}}]},
+    ]}, function(err, item) {
+      if (err) {
+        res.status(500).json({errorType: 'serverError'});
+        console.log('Couldn\'t get element from the db', err);
+        return;
       }
-      db.close();
-    });
+      checkField(db, item, req, res);   
   });
 }
 
 function signUp(req, res) {
   MongoClient.connect(url, function(err, db) {
-    let users = db.collection('users');
-
     /* eslint no-console: 'off' */
     if (err) {
       res.status(500).json({errorType: 'serverError'});
       console.log('Couldn\'t get connect to the db', err);
-      db.close();
       return;
     }
-    users.findOne({
-      $or: [
-        {$and: [{'username': req.username}, {'username': {$ne: ''}}]},
-        {'email': req.email},
-        {$and: [{'phonenumber': req.phonenumber}, {'phonenumber': {$ne: ''}}]},
-      ]}, function(err, item) {
-      if (err) {
-        res.status(500).json({errorType: 'serverError'});
-        console.log('Couldn\'t get element from the db', err);
-        db.close();
-        return;
-      }
-      if (item === null) {
-        users.insert(req, function() {
-          if (err) {
-            res.status(500).json({errorType: 'serverError'});
-            console.log('Couldn\'t insert the element into the db', err);
-            db.close();
-            return;
-          }
-          let objectId = req._id;
-          res.set('location', '/api/signup/' + objectId);
-          res.status(201).json();
-          db.close();
-          return;
-        });
-      } else if (item.email === req.email) {
-        res.status(409).json({errorType: 'emailError'});
-        db.close();
-        return;
-      } else if (item.username === req.username && item.username !== '') {
-        res.status(409).json({errorType: 'usernameError'});
-        db.close();
-        return;
-      } else if (item.phonenumber === req.phonenumber) {
-        res.status(409).json({errorType: 'phoneError'});
-        db.close();
-        return;
-      }
-    });
+    findUser(db, req, res);   
   });
 }
 
