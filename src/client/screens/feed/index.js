404d82c9d77a797ea8c879fddd194b739252cee3
aliceshen227 <Alice_Shen1@epam.com>
2017-10-16 14:12:20 +0800

    succeed

---
+++ src/client/screens/feed/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import ReactDOM from 'react-dom'; // eslint-disable-line no-unused-vars
+import React from 'react';
+import Header from '../../components/Header';
+
+class FeedPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header isLoggedIn={true}/>
+
+
+        
+      </div>
+    );
+  }
+}
+
+export default FeedPage;
