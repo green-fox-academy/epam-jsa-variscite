8af8604abfa208fa385aeb3e5554096556141a36
Chase <chase.wang@hotmail.co.uk>
2017-10-16 20:48:30 +0800

    Post component css lint issue

--- src/client/components/PostInfo/index.js
+++ src/client/components/PostInfo/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 class PostInfo extends React.Component {
   render() {
     return (
-      <div className="postInfo">
+      <div className="post-info">
         <span className="likes">{this.props.postInfo.numOfLikes} likes</span>
         <span>{this.props.postInfo.numOfComments} comments</span>
         <span>{this.props.postInfo.numOfShares} shares</span>
