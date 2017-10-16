ae19d1cd774d7658eb24646378faec794745a193
aliceshen227 <Alice_Shen1@epam.com>
2017-10-16 16:34:13 +0800

    added small changes

--- src/client/components/CommentInfo/index.js
+++ src/client/components/CommentInfo/index.js
@@ -6,9 +6,7 @@ class CommentInfo extends React.Component {
       <div>
         <p className="text">
           <span className="name">{this.props.commentUsername} </span>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliqt,
-          sed diam vtua. At accusam et.
+          {this.props.commentText}
         </p>
         <button className="like-button">Like</button><span> · </span>
         <button>Reply</button><span> · </span>
