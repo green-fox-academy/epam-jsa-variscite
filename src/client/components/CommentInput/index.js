2ac0b360452890573ec82a6d7e796192ed025968
aliceshen227 <Alice_Shen1@epam.com>
2017-10-16 14:59:17 +0800

    finish changing the file with change request

--- src/client/components/CommentInput/index.js
+++ src/client/components/CommentInput/index.js
@@ -4,9 +4,9 @@ class CommentInput extends React.Component {
 
   render() {
     return (
-      <div className='commentInput'>
-        <img className='userPic' src={this.props.myPicURL} />
-        <input placeholder='post a comment'></input>
+      <div className="commentInput">
+        <img className="userPic" src={this.props.myPicURL} />
+        <textarea placeholder="post a comment"></textarea>
       </div>
     );
   }
