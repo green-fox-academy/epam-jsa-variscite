70a65cf57d0246680da3beeeb1cf6da855e4623e
Chase <chase.wang@hotmail.co.uk>
2017-10-16 11:34:40 +0800

    create post component

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Signup from './screens/SignupScreen/index.js';
-import FeedPage from './screens/Feed/index.js';
 
 class App extends React.Component {
   render() {
@@ -10,7 +9,6 @@ class App extends React.Component {
         <div>
           <Route exact path="/" component={Signup} />
           <Route path="/signup" component={Signup} />
-          <Route path="/feed" component={FeedPage} />
         </div>
       </Router>
     );
--- src/client/components/Post/index.js
+++ src/client/components/Post/index.js
@@ -5,14 +5,27 @@ import PostInfo from '../PostInfo/';
 import PostAct from '../PostAct/';
 
 class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.PostInfo = {
+      Username: 'Donald Trump',
+      PostText: 'Make America great again! #America #greatwall',
+      PostTime: '10th Oct at 8:12PM',
+      UserPicURL: 'https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/05/12/104466932-PE_Color.240x240.jpg?v=1494613853',
+      PostPicURL: 'http://ronpaulinstitute.org/media/121032/donald-trumps-mexico-border-wall-557313.jpg',
+      NumOfLikes: 248,
+      NumOfComments: 36,
+      NumOfShares: 192,
+    };
+  }
   render() {
     return (
       <div className="postContainer">
         <section className="postBox">
-          <PostHeader UserInfo={this.props.PostInfo} />
-          <p className="postText">{this.props.PostInfo.PostText}</p>
-          <img className="postPic" src={this.props.PostInfo.PostPicURL} />
-          <PostInfo PostInfo={this.props.PostInfo}/>
+          <PostHeader UserInfo={this.PostInfo} />
+          <p className="postText">{this.PostInfo.PostText}</p>
+          <img className="postPic" src={this.PostInfo.PostPicURL} />
+          <PostInfo PostInfo={this.PostInfo}/>
           <PostAct />
         </section>
       </div>
