e9d5c876416d8ca2063c0873ea30a999acab0425
Chase <chase.wang@hotmail.co.uk>
2017-10-16 14:32:13 +0800

    delete post component from feed

--- src/client/screens/feed/index.js
+++ src/client/screens/feed/index.js
@@ -3,14 +3,12 @@
 import ReactDOM from 'react-dom'; // eslint-disable-line no-unused-vars
 import React from 'react';
 import Header from '../../components/Header';
-import Post from '../../components/Post';
 
 class FeedPage extends React.Component {
   render() {
     return (
       <div>
         <Header isLoggedIn={true}/>
-        <Post />
       </div>
     );
   }
