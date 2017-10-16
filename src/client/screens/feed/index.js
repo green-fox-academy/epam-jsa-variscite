b33b2af4c1d5eade8b685590b6ebab155976888f
Chase <chase.wang@hotmail.co.uk>
2017-10-16 20:22:49 +0800

    add box shadow/ remove border for buttons

--- src/client/screens/feed/index.js
+++ src/client/screens/feed/index.js
@@ -3,12 +3,16 @@
 import ReactDOM from 'react-dom'; // eslint-disable-line no-unused-vars
 import React from 'react';
 import Header from '../../components/Header';
+import AddPost from '../../components/AddPost'
 
 class FeedPage extends React.Component {
   render() {
     return (
       <div>
         <Header isLoggedIn={true}/>
+        <div className='container'>
+          <AddPost />
+        </div>
       </div>
     );
   }
