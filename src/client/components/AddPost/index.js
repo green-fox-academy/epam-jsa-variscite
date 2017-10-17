07a584a1db3a6891ceba64578c74048067aeba91
Chase <chase.wang@hotmail.co.uk>
2017-10-17 12:23:56 +0800

    fixed css and name issues

--- src/client/components/AddPost/index.js
+++ src/client/components/AddPost/index.js
@@ -5,25 +5,24 @@ class AddPost extends React.Component {
   render() {
     return (
       <div className="compose-container">
-        <img src={this.props.MyPicURL} />
-        <form required className="compose">
-          <textarea type="text" name="input" required
+        <img src={this.props.myPicURL} />
+        <form className="compose">
+          <textarea name="input" required
             placeholder="What's on your mind?"></textarea>
           <div className="act">
             <div>
-              <button className="photo">photo</button>
-              <button className="checkin">check in</button>
-              <button className="tag">tag friend</button>
+              <button className="photo" type="button">photo</button>
+              <button className="checkin" type="button">check in</button>
+              <button className="tag" type="button">tag friend</button>
             </div>
             <input type="submit" value="Post"></input>
           </div>
-
         </form>
       </div>
     );
   }
 }
 
-AddPost.defaultProps = {MyPicURL: 'https://pixel.nymag.com/imgs/daily/vulture/2016/08/11/11-obama-sex-playlist.w190.h190.2x.jpg'};
+AddPost.defaultProps = {myPicURL: 'https://pixel.nymag.com/imgs/daily/vulture/2016/08/11/11-obama-sex-playlist.w190.h190.2x.jpg'};
 
 export default AddPost;
