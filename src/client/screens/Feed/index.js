0ebd1f79bb19cb57ea002cee484a70cd2eea95fe
aliceshen227 <Alice_Shen1@epam.com>
2017-10-17 17:11:37 +0800

    added components in feed screen

--- src/client/screens/Feed/index.js
+++ src/client/screens/Feed/index.js
@@ -3,12 +3,18 @@
 import ReactDOM from 'react-dom'; // eslint-disable-line no-unused-vars
 import React from 'react';
 import Header from '../../components/Header';
+import Post from '../../components/Post';
+import Comment from '../../components/Comment';
+import AddPost from '../../components/AddPost';
 
 class FeedPage extends React.Component {
   render() {
     return (
       <div>
         <Header isLoggedIn={true}/>
+        <AddPost />
+        <Post />
+        <Comment />
       </div>
     );
   }
