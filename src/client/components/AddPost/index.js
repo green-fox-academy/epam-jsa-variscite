27076dcd4c1a0d492841ebfd1ba69cbf49e37ebc
aliceshen227 <Alice_Shen1@epam.com>
2017-10-18 17:37:22 +0800

    finish addpost frontend, working on get post

--- src/client/components/AddPost/index.js
+++ src/client/components/AddPost/index.js
@@ -2,11 +2,45 @@ import React from 'react';
 import './style.scss';
 
 class AddPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'errorMessage': null
+    };
+  }
+
+  post(event) {
+    event.preventDefault();
+    let postContent = {content: event.target.elements.namedItem('input').value};
+    if(postContent.content.length > 2){
+      this.sendPost(postContent);
+    } else {
+      this.setState({'errorMessage': 'Please enter more words!'});
+    }
+  }
+
+  sendPost(data) {
+    let xhr = new XMLHttpRequest();
+    let token = window.localStorage.token;
+
+    xhr.addEventListener('readystatechange', function() {
+      if(xhr.readyState === XMLHttpRequest.DONE) {
+        window.location.reload();
+      }
+    }.bind(this));
+    xhr.open('POST', '/api/post');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', token);
+    xhr.send(JSON.stringify(data));
+  }
+
   render() {
     return (
       <div className="compose-container">
         <img src={this.props.myPicURL} />
-        <form className="compose">
+        <form className="compose"
+          onSubmit={this.post.bind(this)}>
           <textarea name="input" required
             placeholder="What's on your mind?"></textarea>
           <div className="act">
@@ -15,6 +49,8 @@ class AddPost extends React.Component {
               <button className="checkin" type="button">check in</button>
               <button className="tag" type="button">tag friend</button>
             </div>
+            {this.state.errorMessage !== null ? <span className="error">
+              {this.state.errorMessage}</span> : null}
             <input type="submit" value="Post"></input>
           </div>
         </form>
--- src/client/screens/Feed/index.js
+++ src/client/screens/Feed/index.js
@@ -8,13 +8,44 @@ import Comment from '../../components/Comment';
 import AddPost from '../../components/AddPost';
 
 class FeedPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  getAllPosts() {
+    
+    this.setState({posts:[{
+      username: 'Donald Trump',
+      postText: 'Make America great again! #America #greatwall',
+      postTime: '10th Oct at 8:12PM',
+      userPicURL: 'https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/05/12/104466932-PE_Color.240x240.jpg?v=1494613853',
+      postPicURL: 'http://ronpaulinstitute.org/media/121032/donald-trumps-mexico-border-wall-557313.jpg',
+      numOfLikes: 248,
+      numOfComments: 36,
+      numOfShares: 192,
+    }]});
+  }
+
+  handleOnload(event){
+    this.getAllPosts();
+  }
+
   render() {
+    var postsToRender = this.state.posts;
+  		postsToRender = postsToRender.map(function(item, key){
+  			return (
+  				<Post item={item} key={key}/>
+  			);
+  		}.bind(this));
     return (
       <div>
         <Header isLoggedIn={true}/>
-        <main className="container">
+        <main className="container" onLoad={this.handleOnload.bind(this)}>
           <AddPost />
-          <Post />
+          {postsToRender}
           <Comment />
         </main>
       </div>
