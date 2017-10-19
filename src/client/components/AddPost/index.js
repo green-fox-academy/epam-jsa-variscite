5134c02ad1813c5e022a3d9c6ec3012a17d2b0bf
aliceshen227 <Alice_Shen1@epam.com>
2017-10-19 14:01:34 +0800

    changed file structure and move all the addpost function to feed screen

--- src/client/components/AddPost/index.js
+++ src/client/components/AddPost/index.js
@@ -1,64 +1,14 @@
 import React from 'react';
 import './style.scss';
-import HTTP_STATUSES from '../../modules/httpStatuses';
+import HTTP_STATUSES from '../../httpStatuses';
 
 class AddPost extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      'errorMessage': null
-    };
-  }
-
-  post(event) {
-    event.preventDefault();
-    let postContent = {content: event.target.elements.namedItem('input').value};
-    if(postContent.content.length > 2){
-      this.sendPost(postContent);
-    } else {
-      this.setState({'errorMessage': 'Please enter more words!'});
-    }
-  }
-
-  handlePostError(status, res) {
-    if(status === BAD_REQUEST) {
-      errorMessage = 'Something went wrong, please try later!';
-      this.setState({'errorMessage': errorMessage});
-    } else if (status === UNAUTHORIZED) {
-      errorMessage = 'You are not authorized! Please log in first!';
-      this.setState({'errorMessage': errorMessage});
-    } else if (status === FORBIDDEN) {
-      errorMessage = 'Please enter more words!';
-      this.setState({'errorMessage': errorMessage});
-    } else if (status === SERVER_ERROR) {
-      errorMessage = 'Sorry! Cannot connect to the database, please try again later!';
-      this.setState({'errorMessage': errorMessage});
-    }
-  }
-
-  sendPost(data) {
-    let xhr = new XMLHttpRequest();
-    let token = window.localStorage.token;
-
-    xhr.addEventListener('readystatechange', function() {
-      if(xhr.readyState === XMLHttpRequest.DONE) {
-        this.handlePostError(xhr.status, xhr.response);
-        window.location.reload();
-      }
-    }.bind(this));
-    xhr.open('POST', '/api/post');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Authorization', token);
-    xhr.send(JSON.stringify(data));
-  }
-
   render() {
     return (
       <div className="compose-container">
         <img src={this.props.myPicURL} />
         <form className="compose"
-          onSubmit={this.post.bind(this)}>
+          onSubmit={this.props.onSubmit}>
           <textarea name="input" required
             placeholder="What's on your mind?"></textarea>
           <div className="act">
@@ -67,8 +17,8 @@ class AddPost extends React.Component {
               <button className="checkin" type="button">check in</button>
               <button className="tag" type="button">tag friend</button>
             </div>
-            {this.state.errorMessage !== null ? <span className="error">
-              {this.state.errorMessage}</span> : null}
+            {this.props.errorMessage !== null ? <span className="error">
+              {this.props.errorMessage}</span> : null}
             <input type="submit" value="Post"></input>
           </div>
         </form>
--- src/client/screens/Feed/index.js
+++ src/client/screens/Feed/index.js
@@ -6,13 +6,27 @@ import Header from '../../components/Header';
 import Post from '../../components/Post';
 import Comment from '../../components/Comment';
 import AddPost from '../../components/AddPost';
+import HTTP_STATUSES from '../../httpStatuses';
 
 class FeedPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      'errorMessage': null
     };
+  }
+
+  handleGetPostError(state) {
+    if(status === HTTP_STATUSES.BAD_REQUEST) {
+      errorMessage = 'Something went wrong, please try later!';
+      this.setState({'errorMessage': errorMessage});
+    } else if (status === HTTP_STATUSES.UNAUTHORIZED) {
+        errorMessage = 'You are not authorized! Please log in first!';
+    } else if (status === HTTP_STATUSES.SERVER_ERROR) {
+      errorMessage = 'Sorry! Cannot connect to the database, please try again later!';
+      this.setState({'errorMessage': errorMessage});
+    }
   }
 
   getAllPosts() {
@@ -21,9 +35,10 @@ class FeedPage extends React.Component {
 
     xhr.addEventListener('readystatechange', function() {
       if(xhr.readyState === XMLHttpRequest.DONE) {
-        let posts = JSON.parse(xhr.response).posts;
+        this.handleGetPostError(xhr.state);
+        let posts = JSON.parse(xhr.response);
         let currentPost = this.state.posts;
-        currentPost.push(posts);
+        posts.forEach(currentPost.push(posts));
         this.setState(currentPost);
       }
     }.bind(this));
@@ -38,18 +53,61 @@ class FeedPage extends React.Component {
     this.getAllPosts();
   }
 
+  post(event) {
+    event.preventDefault();
+    let postContent = {content: event.target.elements.namedItem('input').value};
+    if(postContent.content.length > 2){
+      this.sendPost(postContent);
+    } else {
+      this.setState({'errorMessage': 'Please enter more words!'});
+    }
+  }
+
+  handlePostError(status) {
+    if(status === HTTP_STATUSES.BAD_REQUEST) {
+      errorMessage = 'Something went wrong, please try later!';
+      this.setState({'errorMessage': errorMessage});
+    } else if (status === HTTP_STATUSES.UNAUTHORIZED) {
+      errorMessage = 'You are not authorized! Please log in first!';
+      this.setState({'errorMessage': errorMessage});
+    } else if (status === HTTP_STATUSES.FORBIDDEN) {
+      errorMessage = 'Please enter more words!';
+      this.setState({'errorMessage': errorMessage});
+    } else if (status === HTTP_STATUSES.SERVER_ERROR) {
+      errorMessage = 'Sorry! Cannot connect to the database, please try again later!';
+      this.setState({'errorMessage': errorMessage});
+    }
+  }
+
+  sendPost(data) {
+    let xhr = new XMLHttpRequest();
+    let token = window.localStorage.token;
+
+    xhr.addEventListener('readystatechange', function() {
+      if(xhr.readyState === XMLHttpRequest.DONE) {
+        this.setState({'errorMessage': null});
+        this.handlePostError(xhr.status);
+      }
+    }.bind(this));
+    xhr.open('POST', '/api/post');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', token);
+    xhr.send(JSON.stringify(data));
+  }
+
   render() {
     var postsToRender = this.state.posts;
   		postsToRender = postsToRender.map(function(item, key){
   			return (
-  				<Post item={item} key={key}/>
+  				<Post item={item} key={item.postId}/>
   			);
   		}.bind(this));
     return (
       <div>
         <Header isLoggedIn={true}/>
         <main className="container" onLoad={this.handleOnload.bind(this)}>
-          <AddPost />
+          <AddPost errorMessage={this.state.errorMessage} onSubmit={this.post.bind(this)}/>
           {postsToRender}
           <Comment />
         </main>
