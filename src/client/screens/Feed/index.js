4b122162f068679a20ee1db857eb87684a9afcc6
aliceshen227 <Alice_Shen1@epam.com>
2017-10-23 11:42:23 +0800

    changed the date formate

--- src/client/screens/Feed/index.js
+++ src/client/screens/Feed/index.js
@@ -67,10 +67,25 @@ class FeedPage extends React.Component {
           let posts = JSON.parse(xhr.response).post;
 
           posts.reverse(posts.timeStamp);
-          posts = posts.map(function(item, index){
-            let newDate = new Date(item.timeStamp)
+          posts = posts.map(function(item, index) {
+            let newDate = new Date(item.timeStamp);
+            let months = [
+              'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+            ];
+            let dates = [
+              '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th',
+              '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th',
+              '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th',
+              '26th', '27th', '28th', '29th', '30th', '31st',
+            ];
+            let year = newDate.getFullYear();
+            let month = months[newDate.getMonth()];
+            let date = dates[newDate.getDate()];
+            let hour = newDate.getHours();
+            let min = newDate.getMinutes();
 
-            item.timeStamp = newDate.toString();
+            item.timeStamp = date + ' ' + month + ' ' + year + ' at ' + hour + ':' + min;
             return item;
           });
 
