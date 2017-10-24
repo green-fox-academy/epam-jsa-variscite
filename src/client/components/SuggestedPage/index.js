671b07cc22da8aa88d1b068a40e0f52766200add
aliceshen227 <Alice_Shen1@epam.com>
2017-10-24 17:48:58 +0800

    added the new files

---
+++ src/client/components/SuggestedPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SuggestedPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: [],
+    }
+  }
+  render() {
+    return (
+      <div className="suggested-page">
+
+      </div>
+    );
+  }
+}
+
+export default SuggestedPage;
