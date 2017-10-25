79481e3a55777b3d9600a5d4b473b4bf8b90baed
aliceshen227 <Alice_Shen1@epam.com>
2017-10-25 15:59:23 +0800

    apply changes to the css and made the two component react functional component

--- src/client/components/PagePreview/index.js
+++ src/client/components/PagePreview/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-class SuggestedPage extends React.Component {
-  render() {
-
-    return (
-      <div className="page-preview">
-        <img src={this.props.item.pageImg} />
-        <span>{this.props.item.pageTitle}</span>
-        <p>{this.props.item.pageDetail}</p>
-      </div>
-    );
-  }
-}
+var SuggestedPage = function(props){
+  return (
+    <div className="page-preview">
+      <img src={props.item.pageImg} />
+      <span>{props.item.pageTitle}</span>
+      <p>{props.item.pageDetail}</p>
+    </div>
+  );
+};
 
 export default SuggestedPage;
--- src/client/components/SuggestedPage/index.js
+++ src/client/components/SuggestedPage/index.js
@@ -3,42 +3,38 @@ import PagePreview from '../PagePreview';
 import {Link} from 'react-router-dom';
 import './style.scss';
 
-class SuggestedPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pages: [
-        {
-          id: 1,
-          pageImg: 'http://static.adweek.com/adweek.com-prod/wp-content/uploads/files/white-house-logo-hed-2016.jpg',
-          pageTitle: 'White House Community',
-          pageDetail: '1,977,224,566 likes. 25,888,456,900 talking about this.'
-        },
-        {
-          id: 2,
-          pageImg: 'http://static.adweek.com/adweek.com-prod/wp-content/uploads/files/white-house-logo-hed-2016.jpg',
-          pageTitle: 'White House Community',
-          pageDetail: '1,977,224,566 likes. 25,888,456,900 talking about this.'
-        }
-      ],
-    }
-  }
-  render() {
-    let pagesToRender = this.state.pages;
+var SuggestedPage = function(props){
+  let pagesToRender = props.pages;
 
-    pagesToRender = pagesToRender.map(function(item, key) {
-      return (
-        <PagePreview item={item} key={item.id}/>
-      );
-    });
+  pagesToRender = pagesToRender.map(function(item, key) {
     return (
-      <div className="suggested-page">
-        <h1>My Page</h1>
-        {pagesToRender}
-        <Link to='/createNewPage'>Create a Page</Link>
-      </div>
+      <PagePreview item={item} key={item.id}/>
     );
-  }
-}
+  });
+  return (
+    <div className="suggested-page">
+      <h1>My Page</h1>
+      {pagesToRender}
+      <Link to='/createNewPage'>Create a Page</Link>
+    </div>
+  );
+};
+
+SuggestedPage.defaultProps = {pages:
+  [
+    {
+      id: 1,
+      pageImg: 'http://static.adweek.com/adweek.com-prod/wp-content/uploads/files/white-house-logo-hed-2016.jpg',
+      pageTitle: 'White House Community',
+      pageDetail: '1,977,224,566 likes. 25,888,456,900 talking about this.'
+    },
+    {
+      id: 2,
+      pageImg: 'http://static.adweek.com/adweek.com-prod/wp-content/uploads/files/white-house-logo-hed-2016.jpg',
+      pageTitle: 'White House Community',
+      pageDetail: '1,977,224,566 likes. 25,888,456,900 talking about this.'
+    }
+  ]
+};
 
 export default SuggestedPage;
