54c241199205bd61fa3d3794b4dc153fdca420ff
Vincent Liu <vxiao2046@gmail.com>
2017-10-25 16:37:17 +0800

    share init

--- src/client/components/Post/index.js
+++ src/client/components/Post/index.js
@@ -12,6 +12,8 @@ class Post extends React.Component {
       numOfLikes: this.props.item.numOfLikes,
       errorMessage: null,
       likeState: 'like',
+      numOfShares: this.props.item.numOfShares,
+      errorMessageForShare: null,
     };
   }
 
@@ -20,6 +22,22 @@ class Post extends React.Component {
       let data = JSON.parse(xhr.response);
 
       this.setState({numOfLikes: data.numberOfLikes, likeState: data.isUserLiked ? 'liked' : 'like'});
+    } else if (xhr.status === HTTP_STATUSES.UNAUTHORIZED) {
+      this.setState(
+        {errorMessage: 'Sorry, you are not authorized, please log in first!'}
+      );
+    } else {
+      this.setState(
+        {errorMessage: 'Sorry, Server Error! Please try again later!'}
+      );
+    }
+  }
+
+  handleErrorShare(xhr) {
+    if (xhr.status === HTTP_STATUSES.OK) {
+      let data = JSON.parse(xhr.response);
+
+      this.setState({numOfShares: data.numberOfShares});
     } else if (xhr.status === HTTP_STATUSES.UNAUTHORIZED) {
       this.setState(
         {errorMessage: 'Sorry, you are not authorized, please log in first!'}
@@ -47,6 +65,22 @@ class Post extends React.Component {
     xhr.send(null);
   }
 
+  share(event) {
+    let xhr = new XMLHttpRequest();
+    let token = window.localStorage.getItem('token');
+
+    xhr.addEventListener('readystatechange', function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        this.handleErrorShare(xhr);
+      }
+    }.bind(this));
+    xhr.open('PUT', '/api/post/' + this.props.item._id + '/share');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', token);
+    xhr.send(null);
+  }
+
   render() {
     return (
       <div className="post-container">
@@ -64,6 +98,7 @@ class Post extends React.Component {
             onLikeClick={this.like.bind(this)}
             likeStatus={this.state.likeState}
             showInput = {() => this.props.showInput()}
+            onShareClick={this.share.bind(this)}
           />
         </section>
       </div>
